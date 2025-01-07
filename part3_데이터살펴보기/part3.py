f8032a7882088ce2abdeb28587b58f0c389740ca
sky2000sh <sky2000sh@gmail.com>
2025-01-07 19:34:16 +0900

    Finished part3 & starting part4 (visualisation tools)

--- part3_데이터살펴보기/part3.py
+++ part3_데이터살펴보기/part3.py
@@ -9,119 +9,25 @@ import seaborn as sns
 
 # print('\n')
 
-# 데이터 살펴보기
-# read_csv() 함수로 df 생성
-df = pd.read_csv(r'./auto-mpg.csv', header=None)
-
-# 열 이름 지정
-df.columns= ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight',
-             'acceleration', 'model year', 'origin', 'name']
-# 데이터프레임 df의 내용을 일부 확인
-print('처음 5개 행 =>')
-print(df.head())
-print('\n')
-print('마지막 5개 행 =>')
-print(df.tail())
-print('\n')
-print('df의 모양과 크기 확인: (행의 개수, 열의 개수)를 튜플로 반환 =>')
-print(df.shape)
-print('\n')
-print('df의 내용 확인 =>')
-print(df.info())
-print('\n')
-print('df의 내용 확인 =>')
-print(df.info())
-print('\n')
-print('df의 자료형 확인 =>')
-print(df.dtypes)
-print('\n')
-print('df의 시리즈(mog 열)의 자료형 확인 =>')
-print(df.mpg.dtypes)
-print('\n')
-print('df의 기술 통계 정보 확인 =>')
-print(df.describe())
-print('\n')
-print(df.describe(include='all'))
-print('\n')
-print('df의 데이터 개수 확인 =>')
-print(df.count())
-print('\n')
-print('df.count()가 반환하는 객체 타입 출력 =>')
-print(type(df.count()))
-print('\n')
-print('데이터프레임 df의 특정 열이 가지고 있는 고유값 확인 =>')
-unique_values = df['origin'].value_counts()
-print(unique_values)
-print('\n')
-print('value_counts 함수가 반환하는 객체 타입 출력 =>')
-print(type(unique_values))
-print('\n')
-
-# 통계 함수
-# 평균값
-print('통계 함수 평균값 =>')
-print(df.mean)
-print('\n')
-print(df['mpg'].mean)
-print(df.mpg.mean)
-print('\n')
-print(df[['mpg', 'weight']].mean)
-print('\n')
-
-# 중간값
-print('통계 함수 중간값 =>')
-print(df.median)
-print('\n')
-print(df['mpg'].median)
-print('\n')
-
-# 최대값
-print('통계 함수 최대값 =>')
-print(df.max)
-print('\n')
-print(df['mpg'].max)
-print('\n')
-
-# 최소값
-print('통계 함수 최소값 =>')
-print(df.min)
-print('\n')
-print(df['mpg'].min)
-print('\n')
-
-# 표준편차
-print('통계 함수 표준편차 =>')
-print(df.std)
-print('\n')
-print(df['mpg'].std)
-print('\n')
-
-# 상관계수
-print('통계 함수 상관계수 =>')
-print(df.corr)
-print('\n')
-print(df[['mpg', 'weight']].corr)
-print('\n')
+# 250107
 
 
-# 선 그래프 그리기
-print('선 그래프를 위한 데이터프레임 변환 =>')
-df1 = pd.read_excel(r'./남북한발전전력량.xlsx', engine='openpyxl')
-df_ns = df1.iloc[[0, 5], 3:]
-df_ns.index = ['South', 'North']
-df_ns.columns = df_ns.columns.map(int)
-print(df_ns.head)
-print('\n')
-print('선 그래프 그리기 =>')
-df_ns.plot()
 
-# 행, 열 전치하여 다시 그리기
-print('행, 열 전치하여 다시 그리기 =>')
-tdf_ns = df_ns.T
-print(tdf_ns.head)
-print('\n')
-tdf_ns.plot()
 
-# 막대 그래프
-print('행, 열 전치하여 막대 그래프 그리기 =>')
-tdf_ns.plot(kind='bar')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ part4_시각화도구/part4.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import openpyxl
+import lxml
+# from bs4 import BeautifulSoup4
+import requests
+import re
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+# matplotlib 한글 폰트 오류 문제 해결
+from matplotlib import font_manager, rc
+font_path = r'./malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+# 윈도우 경우
+rc('font', family=font_name)
+# mac 경우
+# rc('font', family=AppleGothic)
+
+# print('\n')
+
+# 250107
+# 선 그래프
+# Excel 데이터를 데이터프레임으로 변환
+print('Excel 데이터를 데이터프레임으로 변환 =>')
+df = pd.read_excel('시도별 전출입 인구수.xlsx', engine='openpyxl', header=0)
+print('\n')
+
+# 누락값(NaN)을 앞 데이터로 채움(엑셀 양식 병합 부분)
+# df = df.fillna(method='ffill')
+df = df.ffill()
+
+# 서울에서 다른 지역으로 이동한 데이터만 추출하여 정리
+print('서울에서 다른 지역으로 이동한 데이터만 추출하여 정리 =>')
+mask = (df['전출지별'] == '서울특별시') & (df['전입지별'] != '서울특별시')
+df_seoul = df[mask]
+df_seoul = df_seoul.drop(['전출지별'], axis=1)
+df_seoul.rename({'전입지별':'전입지'}, axis=1, inplace=True)
+df_seoul.set_index('전입지', inplace=True)
+print('\n')
+
+# 서울에서 경기도로 이동한 인구 데이터 값만 선택
+print('서울에서 경기도로 이동한 인구 데이터 값만 선택 =>')
+sr_one = df_seoul.loc['경기도']
+print('\n')
+
+# 스타일 서식 지정
+print('스타일 서식 지정 =>')
+print('스타일 종류 : classic, bmh, dark_background, fast, grayscale, seaborn 등')
+print('250107기준 더 많은 스타일 정보 : https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html')
+# 스타일 리스트 출력
+print('스타일 리스트 출력 =>')
+print(plt.style.available)
+plt.style.use('ggplot')
+
+# 그림 사이즈 지정(가로 14인치, 세로 5인치
+print('그림 사이즈 지정(가로 14인치, 세로 5인치 =>')
+plt.figure(figsize=(14, 5))
+# x축 눈금 라벨 회전하기
+print('x축 눈금 라벨 회전하기 =>')
+plt.xticks(rotation='vertical')
+print('\n')
+
+# x축, y축 데이터를 plot 함수에 입력
+print('x축, y축 데이터를 plot 함수에 입력 + 마커표시 추가 =>')
+print('marker 마커 모양 : o 또는 + 또는 * 또는 . 등 이 있음  ')
+print('마커 배경색 => markerfacecolor="green"')
+print('마커 크기 => markersize=10')
+print('선의 색 => color="olive"')
+print('선의 두께 => linewidth=2')
+print('라벨 지정 => label="서울 -> 경기"')
+# plt.plot(sr_one.index, sr_one.vals)
+plt.plot(sr_one.index, sr_one.values, marker='o', markersize=10)
+print('\n')
+
+# 차트 제목 추가
+print('차트 제목 추가 + 제목이름 크기 추가 =>')
+plt.title('서울 -> 경기 인구 이동', size=30)
+print('\n')
+
+# 축 이름 추가
+print('x축, y축 이름 추가 + 제목이름 크기 추가 =>')
+plt.xlabel('기간', size=20)
+plt.ylabel('이동 인구수', size=20)
+print('\n')
+
+# 범례 표시
+print('범례 표시 + 크기 추가 =>')
+plt.legend(labels=['서울 -> 경기'], loc='best', fontsize=15)
+
+plt.show()
+print('\n')
+
+# 스타일 리스트 출력
+print(plt.style.available)
+
+
+
+
+
+
+
+
